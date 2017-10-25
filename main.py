4e9cae33ee11b626f695e33dab79bbc584b67318
Sebastian Safari <Sebastian.Safari@parc.com>
2017-10-25 09:14:29 -0700

    Loading csv file now works. Will replace default cells, and resize accordingly.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# pylint: disable=C0103,C0111,W0614,W0401
+# pylint: disable=C0103,C0111,W0614,W0401,C0200
 from Tkinter import *
 import tkMessageBox
 import tkFileDialog
@@ -9,59 +9,114 @@ class Application(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.grid()
-        self.createWidgets()
+        self.createDefaultWidgets()
 
     def focus_next_window(self, event):
         event.widget.tk_focusNext().focus()
         return "break"
 
-    def createWidgets(self):
-        w, h = 7, 1
-        sizeX = 4
-        sizeY = 6
-        cells = []
-        for i in range(sizeY):
-            cells.append([])
-            for j in range(sizeX):
-                cells[i].append([])
-        
+    def selectall(self, event):
+        event.widget.tag_add("sel","1.0","end")
+        event.widget.mark_set(INSERT, "1.0")
+        event.widget.see(INSERT)
+        return "break"
 
-        for i in range(sizeY):
-            cells.append([])
-            for j in range(sizeX):
+    #TODO: Create bind for arrow keys
+
+    def createDefaultWidgets(self):
+        w, h = 7, 1
+        self.sizeX = 4
+        self.sizeY = 6
+        self.cells = []
+        for i in range(self.sizeY):
+            self.cells.append([])
+            for j in range(self.sizeX):
+                self.cells[i].append([])
+
+        for i in range(self.sizeY):
+            self.cells.append([])
+            for j in range(self.sizeX):
                 tmp = Text(self, width=w, height=h)
                 tmp.bind("<Tab>", self.focus_next_window)
+                tmp.bind("<Control-a>", self.selectall)
                 tmp.grid(padx=0, pady=0, column=j, row=i)
-                cells[i][j] = tmp
+                self.cells[i][j] = tmp
+        self.cells[0][0].focus_force()
+
+        #TODO: Add buttons to create new rows/columns
+
+    def loadCells(self, ary):
+        # remove default cells
+        for i in range(self.sizeY):
+            for j in range(self.sizeX):
+                print str(i) + str(j)
+                self.cells[i][j].destroy()
+        
+        # get the max width of the cells
+        mx = 0
+        for i in range(len(ary)):
+            for j in range(len(ary[0])):
+              if( len(ary[i][j]) >= mx ):
+                mx = len(ary[i][j])
+        w = mx
+
+        # create the new cells
+        for i in range(len(ary)):
+            for j in range(len(ary[0])):
+                # print ary[i][j]
+                tmp = Text(self, width=w, height=1)
+                tmp.bind("<Tab>", self.focus_next_window)
+                tmp.bind("<Control-a>", self.selectall)
+                tmp.insert(END, ary[i][j])
+                tmp.grid(padx=0, pady=0, column=j, row=i)
+                tmp.grid(padx=0, pady=0)
+
+        #TODO: Make headers bold, non-editable?
 
 
 def hello():
     tkMessageBox.showinfo("", "Hello!")
 
 
-def openFile():
+def readFile():
     filename = tkFileDialog.askopenfilename(initialdir=".", title="Select file",
                                             filetypes=(("csv files", "*.csv"), ("all files", "*.*")))
+    ary = []
+    col = -1
+    rows = []
+    # get array size & get contents of rows
     with open(filename, "rb") as csvfile:
         rd = csv.reader(csvfile, delimiter=",", quotechar="|")
         for row in rd:
-            print row
+            ary.append([])
+            col = len(row)
+            rows.append(row)
 
+    # create the array
+    for i in range(len(ary)):
+        for j in range(col):
+            ary[i].append([])
 
-root = Tk()
+    # fill the array
+    for i in range(len(ary)):
+        for j in range(col):
+            # print rows[i][j]
+            ary[i][j] = rows[i][j]
 
-menubar = Menu(root)
-
-filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label="New", command=hello)
-filemenu.add_command(label="Open", command=openFile)
-filemenu.add_command(label="Save as", command=hello)
-filemenu.add_command(label="Exit", command=root.quit)
-
-menubar.add_cascade(label="File", menu=filemenu)
+    app.loadCells(ary)
 
 
 app = Application()
+menubar = Menu(app)
+
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="New", command=hello)
+filemenu.add_command(label="Open", command=readFile)
+filemenu.add_command(label="Save as", command=hello)
+filemenu.add_command(label="Exit", command=app.quit)
+
+menubar.add_cascade(label="File", menu=filemenu)
+
 app.master.title('CSV Editor')
 app.master.config(menu=menubar)
 app.mainloop()
